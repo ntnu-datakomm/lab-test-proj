12d8ee29305ab2db1699cde4391e41f3688c4ae9
Girts <girts.strazdins@gmail.com>
2021-09-23 19:10:08 +0200

    Add a text-area and periodically add messages to it

--- src/main/java/no/ntnu/lab/GuiController.java
+++ src/main/java/no/ntnu/lab/GuiController.java
@@ -2,21 +2,51 @@ package no.ntnu.lab;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GuiController {
     @FXML
     Button connectBtn;
     @FXML
     Button sendBtn;
+    @FXML
+    TextArea areaMessages;
+    // Store the buffered message content here
+    StringBuilder messageContent = new StringBuilder();
+
+    Timer messageTimer = new Timer();
 
     public void initialize() {
+        setMouseClickListeners();
+        startTimedTasks();
+
+        System.out.println("GUI Controller initialized");
+    }
+
+    private void startTimedTasks() {
+        messageTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                printGuiMessage("Timer fired");
+            }
+        }, 0, 3000);
+    }
+
+    private void setMouseClickListeners() {
         connectBtn.setOnMouseClicked(event -> {
             System.out.println("Connection NOT IMPLEMENTED!");
         });
         sendBtn.setOnMouseClicked(event -> {
             System.out.println("Message sending NOT IMPLEMENTED!");
         });
+    }
 
-        System.out.println("GUI Controller initialized");
+    private void printGuiMessage(String message) {
+        messageContent.append(message);
+        messageContent.append("\n");
+        areaMessages.setText(messageContent.toString());
     }
 }
